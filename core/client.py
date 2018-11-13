6e7954c456da4b8606d0a3d8c0b39cbc92c14fe1
Bennet Meyers <bennetm@stanford.edu>
2018-11-12 21:34:43 -0800

    job submit works with pandas read

--- core/client.py
+++ core/client.py
@@ -1,8 +1,8 @@
 from sys import path
 from os.path import expanduser
-path.append('/home/ubuntu/StatisticalClearSky/')
-from clearsky.main import IterativeClearSky, ProblemStatusError, fix_time_shifts
-from clearsky.utilities import CONFIG1
+#path.append('/home/ubuntu/StatisticalClearSky/')
+#from clearsky.main import IterativeClearSky, ProblemStatusError, fix_time_shifts
+#from clearsky.utilities import CONFIG1
 import pp
 import numpy as np
 import s3fs
@@ -19,24 +19,22 @@ TZ_LOOKUP = {
     'Pacific/Honolulu': 10
 }
 
+CONFIG1 = TZ_LOOKUP
+
 def load_sys(n, fp=None, verbose=False):
     if fp is not None:
         base = fp
     else:
         base = 's3://pvinsight.nrel/PVO/'
-    try:
-        meta = pd.read_csv('local_data/sys_meta.csv')
-    except FileNotFoundError:
-        meta = pd.read_csv(base + 'sys_meta.csv')
-        meta.to_csv('local_data/sys_meta.csv')
+    meta = pandas.read_csv(base + 'sys_meta.csv')
     id = meta['ID'][n]
-    df = pd.read_csv(base+'PVOutput/{}.csv'.format(id), index_col=0,
+    df = pandas.read_csv(base+'PVOutput/{}.csv'.format(id), index_col=0,
                       parse_dates=[0], usecols=[1, 3])
     tz = meta['TimeZone'][n]
     df.index = df.index.tz_localize(tz).tz_convert('Etc/GMT+{}'.format(TZ_LOOKUP[tz]))   # fix daylight savings
     start = df.index[0]
     end = df.index[-1]
-    time_index = pd.date_range(start=start, end=end, freq='5min')
+    time_index = pandas.date_range(start=start, end=end, freq='5min')
     df = df.reindex(index=time_index, fill_value=0)
     if verbose:
         print(n, id)
@@ -74,53 +72,22 @@ def calc_deg(n, config):
     }
     return output
 
-def isprime(n):
-    """Returns True if n is prime and False otherwise"""
-    if not isinstance(n, int):
-        raise TypeError("argument passed to is_prime is not of 'int' type")
-    if n < 2:
-        return False
-    if n == 2:
-        return True
-    max = int(math.ceil(math.sqrt(n)))
-    i = 2
-    while i <= max:
-        if n % i == 0:
-            return False
-        i += 1
-    return True
-
-def sum_primes(n, config):
-    """Calculates sum of all primes below given integer n"""
-    return sum([x for x in range(2,n) if isprime(x)])
-
-def calc_deg2(n, config):
-    output = {
-        'deg': np.random.random(),
-        'res-median': np.random.random(),
-        'res-var': np.random.random(),
-        'res-L0norm': np.random.random(),
-        'solver-error': np.random.random(),
-        'f1-increase': np.random.random(),
-        'obj-increase': np.random.random(),
-        'fix-ts': np.random.random()
-    }
-    return output
 
 def main(ppservers, pswd, fn, partial=True):
     if partial:
         file_indices = range(200, 204)
     else:
         file_indices = range(573)
-    job_server = pp.Server(ncpus=1, ppservers=ppservers, secret=pswd)
+    job_server = pp.Server(ppservers=ppservers, secret=pswd)
+    from numpy.random import random
     jobs = [
         (
             ind,
             job_server.submit(
-                sum_primes,
-                (ind, CONFIG1),
-                (isprime,),
-                ("math",)
+                load_sys,
+                (ind,),
+                (),
+                ("import pandas",)
             )
         )
         for ind in file_indices
@@ -140,23 +107,21 @@ def main(ppservers, pswd, fn, partial=True):
     # output = pd.DataFrame(columns=['deg', 'res-median', 'res-var', 'res-L0norm', 'solver-error', 'f1-increase',
     #                                'obj-increase', 'fix-ts'])
 
-    # output = pd.DataFrame(columns=['deg', 'res-median', 'res-var', 'res-L0norm', 'solver-error', 'f1-increase',
-    #                                'obj-increase', 'fix-ts'])
-
-    output = pd.DataFrame(columns=['sum_prime'])
+    output = pd.DataFrame(columns=['foo'])
 
     num = len(jobs)
     it = 0
     for ind, job in jobs:
-        output.loc[ind] = job()
+        output.loc[ind] = np.average(job())
         progress(it, num, status='processing files')
         it += 1
     progress(it, num, status='complete              ')
+    print(output)
     home = expanduser('~')
-    with open(home + '.aws/credentials') as f:
+    with open(home + '/.aws/credentials') as f:
         lns = f.readlines()
         key = lns[1].split(' = ')[1].strip('\n')
-        secret =﻿lns[2].split(' = ')[1].strip('\n')
+        secret = lns[2].split(' = ')[1].strip('\n')
     bytes_to_write = output.to_csv(None).encode()
     fs = s3fs.S3FileSystem(key=key, secret=secret)
     with fs.open('s3://pvinsight.nrel/output/' + fn + '.csv', 'wb') as f:
