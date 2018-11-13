489eae5ffec65636a9887f822b4d831b3c02c421
Bennet Meyers <bennetm@stanford.edu>
2018-11-12 18:17:34 -0800

    removing relative imports

--- core/client.py
+++ core/client.py
@@ -2,11 +2,55 @@ from sys import path, argv
 path.append('/home/ubuntu/StatisticalClearSky/')
 from clearsky.main import IterativeClearSky, ProblemStatusError, fix_time_shifts
 from clearsky.utilities import CONFIG1
-from utilities import load_sys, progress
 import pp
 import numpy as np
-import pandas as pd
 import s3fs
+import pandas as pd
+import sys
+
+TZ_LOOKUP = {
+    'America/Anchorage': 9,
+    'America/Chicago': 6,
+    'America/Denver': 7,
+    'America/Los_Angeles': 8,
+    'America/New_York': 5,
+    'America/Phoenix': 7,
+    'Pacific/Honolulu': 10
+}
+
+def load_sys(n, fp=None, verbose=False):
+    if fp is not None:
+        base = fp
+    else:
+        base = 's3://pvinsight.nrel/PVO/'
+    try:
+        meta = pd.read_csv('local_data/sys_meta.csv')
+    except FileNotFoundError:
+        meta = pd.read_csv(base + 'sys_meta.csv')
+        meta.to_csv('local_data/sys_meta.csv')
+    id = meta['ID'][n]
+    df = pd.read_csv(base+'PVOutput/{}.csv'.format(id), index_col=0,
+                      parse_dates=[0], usecols=[1, 3])
+    tz = meta['TimeZone'][n]
+    df.index = df.index.tz_localize(tz).tz_convert('Etc/GMT+{}'.format(TZ_LOOKUP[tz]))   # fix daylight savings
+    start = df.index[0]
+    end = df.index[-1]
+    time_index = pd.date_range(start=start, end=end, freq='5min')
+    df = df.reindex(index=time_index, fill_value=0)
+    if verbose:
+        print(n, id)
+    return df
+
+
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
 
 def calc_deg(n, config):
@@ -51,11 +95,12 @@ def main(ppservers, pswd, fn, partial=True):
     output = pd.DataFrame(columns=['deg', 'res-median', 'res-var', 'res-L0norm', 'solver-error', 'f1-increase',
                                    'obj-increase', 'fix-ts'])
     num = len(jobs)
-    it = 1
+    it = 0
     for ind, job in jobs:
         output.loc[ind] = job()
         progress(it, num, status='processing files')
         it += 1
+    progress(it, num, status='complete')
     output.to_csv( 's3://pvinsight.nrel/output/' + fn)
 
 
